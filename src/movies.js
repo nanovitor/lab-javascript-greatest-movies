0f14d36303ca75164af6e6c2c635e0fafcd08205
Your Name <you@example.com>
2023-01-16 16:48:31 +0000

    done

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,20 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+
+  const directorNames = moviesArray.map(function (element) {
+     return element.director;
+  });
+     return directorNames;
+      }
+
+    //   function getAllDirectors(moviesArray) {
+    //     const directorNames = moviesArray.map((element) => element.director);
+      
+    //     return directorNames;
+    //   }
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {}
@@ -23,3 +36,6 @@ function turnHoursToMinutes(moviesArray) {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
+
+
+
